6dbd26f5c1706317f8257396aca7c7c0b763d0e5
van-fs <van@fullstory.com>
2020-09-17 09:16:05 -0400

    init convert properties

--- src/app/operator/convert-options/convert-options.component.ts
+++ src/app/operator/convert-options/convert-options.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 
 import { OperatorComponent } from '../operator.component';
 import { DataLayerTarget } from '@fullstory/data-layer-observer';
-import { MatSelectChange } from '@angular/material/select';
+import { MatSelectChange, MatSelect } from '@angular/material/select';
 import { MatOption } from '@angular/material/core';
 
 @Component({
@@ -10,7 +10,9 @@ import { MatOption } from '@angular/material/core';
   templateUrl: './convert-options.component.html',
   styleUrls: ['../operator.component.scss']
 })
-export class ConvertOptionsComponent extends OperatorComponent implements OnInit {
+export class ConvertOptionsComponent extends OperatorComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('propertySelect', { static: false }) propertySelect: MatSelect;
 
   properties: string[] = [];
 
@@ -18,13 +20,34 @@ export class ConvertOptionsComponent extends OperatorComponent implements OnInit
 
   ngOnInit() {
     const target = DataLayerTarget.find(this.source);
-    const result = target.query();
 
-    Object.getOwnPropertyNames(result).forEach(property => {
-      if (typeof result[property] !== 'object' && typeof result[property] !== 'function') {
-        this.properties.push(property);
-      }
-    });
+    if (!this.options.properties) {
+      const result = target.query();
+
+      Object.getOwnPropertyNames(result).forEach(property => {
+        if (typeof result[property] !== 'object' && typeof result[property] !== 'function') {
+          this.properties.push(property);
+        }
+      });
+    } else {
+      this.properties = this.options.properties.split(',');
+    }
+  }
+
+  ngAfterViewInit() {
+    if (this.options.properties) {
+      this.propertySelect.options.forEach(option => {
+        if (this.options.properties.indexOf(option.value) > -1) {
+          console.log(option.value)
+
+          try {
+            option.select();
+          } catch (err) {
+            // ignore ExpressionChangedAfterItHasBeenCheckedError: Expression has changed after it was checked.
+          }
+        }
+      });
+    }
   }
 
   selectProperty(event: MatSelectChange) {
